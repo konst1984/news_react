cc3a83f9fad4b836e03e8a7c647c63a49ba67fb7
Konst_V <varjutin@gmail.com>
2023-09-26 23:04:14 +0300

    add filtering by categories

---
+++ src/components/Categories/Categories.jsx
@@ -0,0 +1,31 @@
+import styles from "./styles.module.css";
+
+const Categories = ({ categories, setSelectedCategory, selectedCategory }) => {
+  return (
+    <div className={styles.categories}>
+      <button
+        onClick={() => setSelectedCategory(null)}
+        className={!selectedCategory ? styles.active : styles.item}
+      >
+        All
+      </button>
+      {categories?.map((category) => {
+        return (
+          <button
+            onClick={() => setSelectedCategory(category)}
+            className={
+              selectedCategory === category ? styles.active : styles.item
+            }
+            key={category}
+          >
+            {category}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+// Categories.displayName = "Categories";
+
+export default Categories;
