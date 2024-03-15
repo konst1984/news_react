52a3a87b14c35d02952c6e6a986642ef2983b19e
Konst_V <varjutin@gmail.com>
2024-03-15 20:06:48 +0300

    add news page and react router

--- src/app/appEntry.tsx
+++ src/app/appEntry.tsx
@@ -2,16 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { ThemeProvider } from "@/app/providers/ThemeProvider";
-import BaseLayout from "./layouts/BaseLayout";
 import { store } from "./appStore";
-import './index.css';
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import appRouter from "./appRouter";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <ThemeProvider>
-      <React.StrictMode>
-        <Provider store={store}>
-          <BaseLayout />
-        </Provider>
-      </React.StrictMode>
+        <React.StrictMode>
+            <Provider store={store}>
+                <RouterProvider router={appRouter} />
+            </Provider>
+        </React.StrictMode>
     </ThemeProvider>
 );
--- src/entities/news/model/newsSlice.ts
+++ src/entities/news/model/newsSlice.ts
@@ -7,12 +7,14 @@ import { INews } from './types'
 // Define a type for the slice state
 interface State {
   news: INews[],
+  currentNews: INews | null
   filters: IFilters
 }
 
 // Define the initial state using that type
 const initialState: State = {
   news: [],
+  currentNews: null,
   filters: {
     page_number: 1,
     page_size: PAGE_SIZE,
@@ -28,13 +30,16 @@ export const newsSlice = createSlice({
     setNews: (state, action: PayloadAction<INews[]>) => {
       state.news=action.payload;
     },
+    setCurrentNews: (state, action: PayloadAction<INews | null>) => {
+      state.currentNews = action.payload
+    },
     setFilters: (state, action: PayloadAction<{key: string, value: string | number | null}>) => {
       const {key, value} = action.payload;
       state.filters = {...state.filters,[key]: value}
-    }
+    },
   },
 })
 
-export const { setNews, setFilters } = newsSlice.actions
+export const { setNews, setFilters,setCurrentNews } = newsSlice.actions
 
 export default newsSlice.reducer
